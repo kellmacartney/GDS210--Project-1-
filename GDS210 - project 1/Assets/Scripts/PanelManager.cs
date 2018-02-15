aa46ede1a3cb6919563c62c01e98821611191f4c
kellmacartney <33106247+kellmacartney@users.noreply.github.com>
2018-02-15 18:50:30 +1000

    Scene Manager and Panel Manager
    Scene Management script written but not implemented
    
    Panel Manager SCript written but not implemented

---
+++ GDS210 - project 1/Assets/Scripts/PanelManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PanelManager : MonoBehaviour
+{
+    public GameObject CreditsPanel;
+    public GameObject MenuPanel;
+    public GameObject SplashPanel;
+
+    bool credits = false;
+    bool menu = false;
+    bool splash = true;
+
+    void Update()
+    {
+        if (credits)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowCredits();
+            }
+        }
+        if (menu)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowMenu();
+            }
+        }
+        if (splash)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                ShowSplash();
+            }
+        }
+    }
+    public void ShowCredits()
+    {
+        credits = true;
+        splash = false;
+        menu = false;
+        CreditsPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+        SplashPanel.SetActive(false);
+    }
+
+    public void ShowMenu()
+    {
+        menu = true;
+        credits = false;
+        splash = false;
+        MenuPanel.SetActive(true);
+        SplashPanel.SetActive(false);
+        CreditsPanel.SetActive(false);
+
+    }
+
+    public void ShowSplash()
+    {
+        splash = true;
+        credits = false;
+        menu = false;
+        SplashPanel.SetActive(true);
+        CreditsPanel.SetActive(false);
+        MenuPanel.SetActive(false);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ GDS210 - project 1/Assets/Scripts/Scenemanagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenemanagement : MonoBehaviour
+{
+
+
+    public void LoadLevel(string LeveltoLoad)
+    {
+
+        SceneManager.LoadScene(LeveltoLoad);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
